cdd2a6d273a185b9aa105dc9daa4c5399f14e1a2
R-Bicho <rogerio-bicho@outlook.com>
2023-06-30 15:01:49 -0300

    ajustando re para aceitar apenas emails TIM

--- blueprints/funcoes/send_email.py
+++ blueprints/funcoes/send_email.py
@@ -31,8 +31,7 @@ def enviandoEmail(email_destino, senha_temporaria):
 
 
 def validandoEmail(email):
-    email_regexp = re.compile(r'^[A-Za-z]*@timbrasil\.com\.br$')
-    email_regexp = re.compile(r'^[A-Za-z]*-([A-Za-z]*)? @outlook\.com$')
+    email_regexp = re.compile(r'^[A-Za-z]*@timbrasil\.com\.br$')    
 
     if email_regexp.search(email):
         return True
